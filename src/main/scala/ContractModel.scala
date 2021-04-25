f6efd7b83b036b6b82928e51ce9a2a3d5cf29055
FMC <franklin@peeriq.com>
2021-04-25 11:42:12 -0400

    add contract model

--- src/main/scala/ContractModel.scala
+++ src/main/scala/ContractModel.scala
@@ -1,4 +1,26 @@
 case class ContractModel(name: String,
-                         atRiskAmount: BigDecimal = BigDecimal(0.13),
+                         atRiskRatio: BigDecimal = BigDecimal(0.13),
                          totalContractValue: BigDecimal = BigDecimal(5000000),
-                         logicalResourceUnits: Set[LRUModel])
+                         logicalResourceUnits: Set[LRUModel]) {
+
+  // Service level credits need to be computed at the contract model level
+  // because the total contract value and at risk amount are at the contract model level
+  val mapServiceLevelCreditToServiceLevel: Map[SLAModel, BigDecimal] =
+    logicalResourceUnits.flatMap { lru =>
+      lru.serviceLevelAgreements.map { sla =>
+        sla -> serviceLevelCreditFormula(sla, lru, BigDecimal(0.4))
+      }
+    }.toMap
+
+  def serviceLevelCreditFormula(s: SLAModel, lru: LRUModel, defaultFactor: BigDecimal): BigDecimal = {
+    val atRiskAmount = atRiskRatio * totalContractValue
+    val cpiAllocationPercentage = s.allocationPercentage
+    val lruAllocationPercentage = lru.allocationPercentage
+    atRiskAmount * lruAllocationPercentage * cpiAllocationPercentage * defaultFactor
+  }
+
+  val serviceLevelCreditTotal: BigDecimal = mapServiceLevelCreditToServiceLevel.values.sum
+
+  val atRiskAmount: BigDecimal = totalContractValue * atRiskRatio
+
+}
